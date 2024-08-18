274df2191af1fac73d90e8651013b3a3053ce597
chibataku0815 <chiba@fores-tone.co.jp>
2024-08-18 17:01:23 +0900

    chore: Update hono module declaration in global.d.ts

--- app/global.d.ts
+++ app/global.d.ts
@@ -3,9 +3,9 @@ import type { Meta } from './types'
 
 declare module 'hono' {
   interface ContextRenderer {
-    (
+    render: (
       content: string | Promise<string>,
       meta?: Meta & { frontmatter: Meta }
-    ): Response | Promise<Response>
+    ) => Response | Promise<Response>
   }
 }
